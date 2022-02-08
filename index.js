8137fd5d54095a7c6c76091a58aec0e7defaf04a
Hernan Dupuy <83312096+PlantaH@users.noreply.github.com>
2022-02-08 11:30:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+
+const { Server: HttpServer } = require('http')
+const { Server: Socket } = require('socket.io')
+
+const contenedor = require("./api/contenedor.js")
+const file = new contenedor('./api/productos.txt')
+
+const contenedorMessages = require("./api_messages/contenedor.js")
+const messages = new contenedorMessages('./api_messages/messages.txt')
+
+const PORT = 8080 || process.env.PORT;
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+ 
+ 
+io.on('connection', async socket => {
+    console.log('Nueva conexion');
+ 
+    socket.emit('productos', file.getAll());
+    
+    socket.on('newItem', producto => {         
+        file.save(producto.title,producto.price,producto.thumb)
+        io.sockets.emit('productos', file.getAll());
+    })
+
+      
+    socket.emit('messages', await messages.getAll());
+ 
+    socket.on('newMessage', async mensaje => {
+        mensaje.fyh = new Date().toLocaleString()
+        mensaje.socketId = socket.id
+        console.log(mensaje);
+        await messages.save(mensaje)
+        io.sockets.emit('messages', await messages.getAll());
+    }) 
+});
+ 
+ 
+const connectedServer = httpServer.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
+})
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
